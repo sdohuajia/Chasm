9f919a849432617c60b77123ed04404c729b6eac
sdohuajia <63113882+sdohuajia@users.noreply.github.com>
2024-07-21 18:41:36 +0800

    Update Chasm.sh

--- Chasm.sh
+++ Chasm.sh
@@ -27,33 +27,33 @@ function install_node() {
 
     # 使用 tee 命令将内容写入 .env 文件
     tee .env > /dev/null <<EOF
-PORT=3001
-LOGGER_LEVEL=debug
+    PORT=3001
+    LOGGER_LEVEL=debug
+    
+    # Chasm
+    ORCHESTRATOR_URL=https://orchestrator.chasm.net
+    SCOUT_NAME=myscout
+    SCOUT_UID=$SCOUT_UID
+    WEBHOOK_API_KEY=$WEBHOOK_API_KEY
+    # Scout Webhook Url, update based on your server's IP and Port
+    # e.g. http://123.123.123.123:3001/
+    WEBHOOK_URL=$WEBHOOK_URL
 
-# Chasm
-ORCHESTRATOR_URL=https://orchestrator.chasm.net
-SCOUT_NAME=myscout
-SCOUT_UID=$SCOUT_UID
-WEBHOOK_API_KEY=$WEBHOOK_API_KEY
-# Scout Webhook Url, update based on your server's IP and Port
-# e.g. http://123.123.123.123:3001/
-WEBHOOK_URL=$WEBHOOK_URL
+    # Chosen Provider (groq, openai)
+    PROVIDERS=groq
+    MODEL=gemma2-9b-it
+    GROQ_API_KEY=$GROQ_API_KEY
 
-# Chosen Provider (groq, openai)
-PROVIDERS=groq
-MODEL=gemma2-9b-it
-GROQ_API_KEY=$GROQ_API_KEY
-
-# Optional
-OPENROUTER_API_KEY=$OPENROUTER_API_KEY
-OPENAI_API_KEY=$OPENAI_API_KEY
+    # Optional
+    OPENROUTER_API_KEY=$OPENROUTER_API_KEY
+    OPENAI_API_KEY=$OPENAI_API_KEY
 EOF
 
     # 输出 .env 文件内容，用于验证
     cat .env
 
     # 提示用户是否退出脚本
-    echo "是否退出？选择no继续(yes/no)"
+    echo "是否退出？ (yes/no)"
     read answer
 
     if [ "$answer" != "no" ]; then
@@ -72,16 +72,21 @@ EOF
     # 从 .env 文件中加载环境变量
     source ./.env
 
+    # 输出消息
+    echo "请求已发送，退出脚本。"
+    exit 0
+}
+
+# 发送 POST 请求到 webhook 的函数
+function send_webhook_request() {
+    source ./.env
+
     # 使用 curl 发送 POST 请求到 webhook
     curl -X POST \
          -H "Content-Type: application/json" \
          -H "Authorization: Bearer $WEBHOOK_API_KEY" \
          -d '{"body":"{\"model\":\"gemma2-9b-it\",\"messages\":[{\"role\":\"system\",\"content\":\"You are a helpful assistant.\"}]}"}' \
          $WEBHOOK_URL
-
-    # 输出消息并退出脚本
-    echo "请求已发送，退出脚本。"
-    exit 0
 }
 
 # 主菜单
@@ -97,10 +102,12 @@ function main_menu() {
         echo "退出脚本，请按键盘ctrl c退出即可"
         echo "请选择要执行的操作:"
         echo "1. 安装节点"
-        read -p "请输入选项（1-3）: " OPTION
+        echo "2. 发送 Webhook 请求"
+        read -p "请输入选项（1-2）: " OPTION
 
         case $OPTION in
         1) install_node ;;
+        2) send_webhook_request ;;
         *) echo "无效选项，请重新输入。" ;;
         esac
         echo "按任意键返回主菜单..."
